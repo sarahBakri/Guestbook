6fd8a28606ac9eb69b11edcfe83d87c9958ce0b1
sarahBakri <sarabakri96@gmail.com>
2020-05-06 06:52:59 +0200

    edit

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-require('./db/mongoose')
-const port=process.env.PORT || 3000
-app.use(express.json())
-app.use(cors())
-app.listen(port,()=>{
-    console.log('server is on port '+port)
-})
